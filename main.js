78ef5751459002abb78f29f0de02766a2b3cc570
Chloe <chloe84@msn.com>
2022-01-22 15:03:12 -0800

    updated information for the draw concept

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
+// RAM = Random Access Memory = variables
+// CPU = Central Processing Unit = logic execution
+
+
+// FPS = 
+
 const debug = document.querySelector('.debug');
 debug.innerText = 'hello world';
+
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+context.fillStyle = 'red';
+context.fillRect(50, 50, 100, 100);
---
+++ readme.md
@@ -0,0 +1,14 @@
+## Mario Gameproject remake
+
+# I want to make a remake of a platform of using javascript and cancas technology
+
+[] create a system that draws my character every frame
+    [] Mario
+
+    [] Enemies
+
+    [] Obstacles
+
+[] give my character the ability to move in any direction 
+
+[] apply gravity to my character
